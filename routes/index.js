ea34eed33d648a70e41e8ae4526d1b64b9ef0767
Andrés Toledano <andres13072001@gmail.com>
2023-02-21 12:49:30 +0100

    Nodemailer terminado, llega el correo correctamente

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer')
 
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure:true,
+  auth:{
+    user: 'andres13072001@gmail.com',
+    pass: 'azgkbzpkjilkdqzo'
+  }
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Segunda app con express' });
@@ -13,5 +24,17 @@ router.get('/contact', function(req, res, next) {
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
+
+
+router.post('/send-email',async(req,res)=>{
+  await transporter.sendMail({
+    from: '"Fred Foo 👻" <andres13072001@gmail.com>', // sender address
+    to: "andres13072001@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    
+  });
+  res.redirect('/send-email')
+})
 
 module.exports = router;
